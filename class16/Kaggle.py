6ba6c998404e928e12db47793d37d0b5d04ea5c5
Jennifer Stark <jastark1@gmail.com>
2015-10-22 18:15:03 -0400

    nlp and reg models...

--- class16/Kaggle.py
+++ class16/Kaggle.py
@@ -6,7 +6,9 @@ Created on Thu Oct  8 19:12:29 2015
 """
 
 import pandas as pd
-train = pd.read_csv('train.csv', index_col=0)
+train = pd.read_csv('../../kaggle/train.csv', index_col=0)
+
+# EXPLORATION
 train.head()
 
 train.OpenStatus.value_counts()
@@ -38,8 +40,8 @@ def make_features(filename):
     df['TitleLength'] = df.Title.apply(len)
     return df
     
-train = make_features('train.csv')
-test = make_features('test.csv')
+train = make_features('../../kaggle/train.csv')
+test = make_features('../../kaggle/test.csv')
 
 feature_cols = ['ReputationAtPostCreation', 'Answers', 'TitleLength']
 X = train[feature_cols]
@@ -99,8 +101,8 @@ def make_features(filename):
     return df
 
 # apply function to both training and testing files
-train = make_features('train.csv')
-test = make_features('test.csv')
+train = make_features('../../kaggle/train.csv')
+test = make_features('../../kaggle/test.csv')
 
 # train the model on ALL data
 feature_cols = ['ReputationAtPostCreation', 'Answers', 'TitleLength', 'NumTags', 'OwnerAge']
@@ -117,7 +119,33 @@ sub.to_csv('sub2.csv')
 
 
 
+'''
+Build a document-term matrix from Title using CountVectorizer
+'''
 
+# build document-term matrix for the training data
+from sklearn.feature_extraction.text import CountVectorizer
+vect = CountVectorizer(stop_words='english')
+dtm = vect.fit_transform(train.Title)
+
+# define X and y
+X = dtm
+y = train.OpenStatus
+
+# build document-term matrix for the actual testing data and make predictions
+oos_dtm = vect.transform(test.Title)
+from sklearn.naive_bayes import MultinomialNB
+nb = MultinomialNB()
+nb.fit(X, y)
+oos_pred_prob = nb.predict_proba(oos_dtm)[:, 1]
+sub = pd.DataFrame({'id':test.index, 'OpenStatus':oos_pred_prob}).set_index('id')
+sub.to_csv('sub3.csv')  # 0.544
+'''
+We used the length of the Title as a feature. How about the length of BodyMarkdown?
+We created a document term matrix for Title. How about doing the same for BodyMarkdown?
+Can you think of any custom features for Title? Like, we theorized that all 
+lowercase titles might be indicative of bad questions. Can you create that as a feature?
+What about using sentiment of the Title or BodyMarkdown as a feature?
 
 
 
